1289bd358a09619ad594d7fe9a04610bf525427f
Andras Fekete <bandi13@users.noreply.github.com>
2021-04-08 21:59:15 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t cbtrader . && \
-docker run --rm -it -v $(pwd):/opt/cbtrader -w /opt/cbtrader cbtrader python3 wsfeed.py
+docker run --rm -it -v $(pwd):/opt/cbtrader -w /opt/cbtrader cbtrader python3 cbtrader.py
