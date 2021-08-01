34b7d91617bc072ca5a53fe6f3aea7ba8e83f22e
Andras Fekete <bandi13@users.noreply.github.com>
2021-08-01 00:38:23 -0400

    Don't need interactive or pty

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build -t cbtrader . && \
-docker run --rm -it -v $(pwd):/opt/cbtrader -w /opt/cbtrader cbtrader python3 cbtrader.py
+docker run --rm -v $(pwd):/opt/cbtrader -w /opt/cbtrader cbtrader python3 cbtrader.py -b
